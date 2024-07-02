f4cc58564395076f57076f95f8543b9d4041d66c
HaseebSardar12 <haseebsardar123123@gmail.com>
2024-07-02 07:50:08 +0500

    This is a dispatch management desktop application made in NetBeans IDE using Java language with file handling.

---
+++ DispatchDesktopApp/src/dispatchdesktopapp/GlobalVariables.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dispatchdesktopapp;
+
+
+public class GlobalVariables {
+    public static final String TRUCKFILE = "trucks.txt";
+    public static final String ITEMFILE = "items.txt";
+    public static final String DISPATCHFILE = "dispatch.txt";
+
+}
